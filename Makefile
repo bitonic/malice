a97443c7e400ef244c84a7f28cd5fd8d5a1ac914
Francesco <e.imhotep@gmail.com>
2010-11-06 17:48:16 +0000

    Edited makefile and main file

--- Makefile
+++ Makefile
@@ -1,13 +1,7 @@
 default:
-	rm -f Parser.hs
-	rm -f Scanner.hs
-	alex Scanner.x
-	happy Parser.y
-	ghc -o malice Scanner.hs Parser.hs CodeCleanup.hs Code2C.hs CodeGen.hs Semantics.hs Malice.hs
+	ghc -o malice Parser.hs CodeCleanup.hs Code2C.hs CodeGen.hs Semantics.hs Malice.hs
 
 clean:
-	rm -f Parser.hs
-	rm -f Scanner.hs
 	rm -f *.hi
 	rm -f *.o
 	rm -f malice
--- Malice.hs
+++ Malice.hs
@@ -10,10 +10,11 @@ import Scanner
 main
   = do [fn] <- getArgs
        f <- readFile fn
-       let ast = maliceParser $ maliceScanner f
-       putStrLn ("Abstract Syntax Tree:\n" ++ (show ast) ++ "\n")
-       sem <- maliceSemantics ast
-       if sem
-         --then putStrLn ("The semantics test was successful.\n\n" ++ "Generated C code:\n" ++ (convertProgramToC ast))
-		 then putStrLn $ show $ llProgram ast
-         else putStrLn "Semantics test failed."
+       let parseResult = maliceParser f in
+         case parseResult of
+           Left e    -> return ()
+           Right ast -> do putStrLn ("Abstract Syntax Tree:\n" ++ (show ast) ++ "\n")
+                           sem <- maliceSemantics ast
+                           if sem
+                             then putStrLn $ show $ llProgram ast
+                             else putStrLn "Semantics test failed."
