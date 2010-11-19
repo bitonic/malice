b5e7fd7d50be63f6ab5499beae60e43fc2d27490
Francesco <e.imhotep@gmail.com>
2010-11-19 23:24:06 +0000

    Loops and perhaps working.

--- Parser.hs
+++ Parser.hs
@@ -3,13 +3,11 @@ module Parser
          maliceParser, maliceParseFile,
          MaliceType(..),
          StatementList, Statement(..), Expr(..),
-         StatementListPos,
          SourcePos, newPos,
-         unPosSL,
        ) where
 
 import Data.Int ( Int32 )
-import Control.Monad ( liftM )
+import Control.Monad ( liftM, liftM2 )
 import Text.ParserCombinators.Parsec
 import Text.ParserCombinators.Parsec.Expr
 import Text.ParserCombinators.Parsec.Token
@@ -24,18 +22,17 @@ data MaliceType = MaliceInt | MaliceChar | MaliceBool
                 deriving (Show, Eq)
 
 type StatementList = [Statement]
-type StatementListPos = [StatementPos]
 
-type StatementPos = (SourcePos, Statement)
-data Statement
+type Statement = (SourcePos, StatementAct)
+data StatementAct
      = Assign String Expr
      | Declare MaliceType String
      | Decrease String
      | Increase String
      | Return Expr
      -- Composite statements
-     | Until Expr StatementListPos
-     | Switch [(Expr, StatementPos)]
+     | Until Expr StatementList
+     | Switch [(Expr, Statement)]
      deriving (Show, Eq)
 
 data Expr
@@ -46,16 +43,12 @@ data Expr
      | Var String
      deriving (Show, Eq)
               
--- Converts from AST with positions to a AST without
-unPosSL :: StatementListPos -> StatementList
-unPosSL sl = map unPosS sl
-
-unPosS (_, s) = s
-              
 -- Language characteristics
 
-operators = ["+", "-", "*", "/", "%", "^", "&"
-             , "|", "~", "=="]
+operators = ["+", "-", "*", "/", "%", "^", "&", "|", "~",
+             "==", "<", ">", "&&", "||"
+            ]
+ 
 
 def = emptyDef { identStart = letter
                , identLetter = alphaNum <|> char '_'
@@ -78,24 +71,23 @@ TokenParser { identifier = p_identifier
             } = makeTokenParser def
 
 -- Actual parser
-mainparser :: Parser StatementListPos
+mainparser :: Parser StatementList
 mainparser = do p_white
-                sl <- many1 p_statementSep
+                sl <- many1 p_statement
                 return sl
 
-p_statementSep = p_statement >>= (\s -> p_separator >> return s)
-
-p_separator = try (p_string "too" >> p_separator')
-          <|> p_separator'
-          <?> "statement separator"
-p_separator' = choice $ map p_string [ "and", "but", "then", ".", ","]
+p_separator s = s <* (try (p_string "too" >> p_separator')
+                      <|> p_separator'
+                      <?> "statement separator")
+  where p_separator' = choice $ map p_string [ "and", "but", "then", ".", ","]
 
 -- Statement
 p_statement = do
   pos <- getPosition
   s <- (try p_return
-        <|> try (p_identifier >>= p_statement_id)
-        <|> p_until
+        <|> try (p_separator $ p_identifier >>= p_statement_id)
+        <|> try (p_separator $ p_until)
+        <|> p_switch
         <?> "statement")
   return (pos, s)
 
@@ -123,34 +115,31 @@ p_until = do
   p_string "eventually"
   e <- p_expr
   p_string "because"
-  sl <- manyTill p_statementSep (try (p_string "enough" >>
-                                 p_string "times"))
-  return $ Until e sl
+  liftM (Until e) $ manyTill p_statement (try (p_string "enough" >>
+                                               p_string "times"))
 
 p_switch = do 
   p_string "perhaps"
   e <- p_expr
   p_string "so"
   s <- p_statement
-  many p_switch' >>= (return $ Switch $ (:) (e, s))
-
-p_switch' = do
-  p_string "or" >> p_string "maybe"
-  e <- p_expr
-  p_string "so"
-  s <- p_statement
-  return (e, s)
-  
+  liftM (Switch . (:) (e, s)) $ many p_switch'
+  where p_switch' = do
+          p_string "or" >> p_string "maybe"
+          liftM2 (,) (p_expr <* p_string "so") p_statement
 
 -- Expression
 p_expr = buildExpressionParser table term <?> "expression"
 table = [ [prefixOp "~"]
         , map infixOp ["*", "/", "%"]
         , map infixOp ["+", "-"]
+        , map infixOp ["<", ">"]
         , [infixOp "=="]
         , [infixOp "&"]
         , [infixOp "^"]
         , [infixOp "|"]
+        , [infixOp "&&"]
+        , [infixOp "||"]
         ]
 
 prefixOp op
@@ -160,10 +149,10 @@ infixOp op
   = Infix (p_reservedOp op >> return (BinOp op)) AssocLeft
 
 term = (lookAhead p_operator >> p_expr)
-   <|> p_parens p_expr
-   <|> liftM Var p_identifier
-   <|> liftM Char p_letter
-   <|> liftM Int p_int32
+       <|> p_parens p_expr
+       <|> liftM Var p_identifier
+       <|> liftM Char p_letter
+       <|> liftM Int p_int32
 
 p_int32 = do
   int <- p_integer
@@ -174,8 +163,14 @@ p_operator = choice $ map p_string operators
 -- Utils
 p_string = p_lexeme . string
 
+p <* q = p >>= (\x -> q >> return x)
+
+-- useful for debugging when you don't want the positions
+showSL :: StatementList -> String
+showSL = show . map snd
+
 -- parser from string
-maliceParser :: String -> String -> Either ParseError StatementListPos
+maliceParser :: String -> String -> Either ParseError StatementList
 maliceParser s f = parse mainparser f s
 
 -- Parse from file
@@ -183,5 +178,5 @@ maliceParseFile :: String -> IO (Either ParseError StatementList)
 maliceParseFile f = do
   s <- readFile f
   case maliceParser s f of
-    (Right sl) -> return (Right $ unPosSL sl)
+    (Right sl) -> return (Right sl)
     (Left err) -> return (Left err)
